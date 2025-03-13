15825e14fd6ff4700f4602966063ec4d42a88fba
Steady-K <tkdlxm56@gmail.com>
2025-03-13 20:05:20 +0900

    feat:Add to-do list

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -14,6 +14,7 @@ export default class App extends Component {
         completed: false,
       },
     ],
+    value: '',
   };
 
   btnStyle = {
@@ -38,6 +39,25 @@ export default class App extends Component {
     this.setState({ todoData: newTodoData });
     console.log('newTodoData', newTodoData);
   };
+
+  handleChange = (e) => {
+    console.log('e', e.target.value);
+    this.setState({ value: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // 새로운 할 일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
+      completed: false,
+    };
+
+    // 원래 있던 할 일에 새로운 할 일 더해주기
+    this.setState({ todoData: [...this.state.todoData, newTodo] });
+  };
   render() {
     return (
       <div className="container">
@@ -58,6 +78,23 @@ export default class App extends Component {
               </button>
             </div>
           ))}
+
+          <form style={{ display: 'flex' }} onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="value"
+              style={{ flex: '10', padding: '5px' }}
+              placeholder="해야 할 일을 입력하세요."
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <input
+              type="submit"
+              value="입력"
+              className="btn"
+              style={{ flex: '1' }}
+            />
+          </form>
         </div>
       </div>
     );
