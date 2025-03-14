0fb996de7f285b3920cf2495296f503a3dbd6c94
Steady-K <tkdlxm56@gmail.com>
2025-03-14 23:12:16 +0900

    feat:bar

--- react-netflix-clone/src/App.js
+++ react-netflix-clone/src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 
-const App = () => {
+function App() {
   return (
     <div className="App">
       <Nav />
     </div>
   );
-};
+}
 
 export default App;
--- react-netflix-clone/src/components/Nav.js
+++ react-netflix-clone/src/components/Nav.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Nav.css';
 
 export default function Nav() {
+  const [show, setshow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        setshow(true);
+      } else {
+        setshow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    };
+  }, []);
+
   return (
-    <nav>
+    <nav className={`nav ${show && 'nav__black'}`}>
       <img
         alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Netflix.png"
@@ -12,7 +28,7 @@ export default function Nav() {
       />
       <img
         alt="User logged"
-        src="https://i.pinimg.com/564x/1b/a2/e6/1ba2e6d1d4874546c70c91f1024e17fb.jpg"
+        src="https://img.freepik.com/premium-vector/funny-green-face-square-avatar-cartoon-emotion-icon_53562-16129.jpg"
         className="nav__avatar"
       />
     </nav>
