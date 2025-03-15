1e6342375334afbb94d7f3ede047daeee4abe127
Steady-K <tkdlxm56@gmail.com>
2025-03-15 16:34:21 +0900

    feat:video banner

--- react-netflix-clone/src/components/Banner.js
+++ react-netflix-clone/src/components/Banner.js
@@ -2,9 +2,11 @@ import axios from '../api/axios';
 import React, { useEffect, useState } from 'react';
 import requests from '../api/requests';
 import './Banner.css';
+import styled from 'styled-components';
 
 export default function Banner() {
   const [movie, setmovie] = useState([]);
+  const [isClicked, setisClicked] = useState(false);
   useEffect(() => {
     fetchData();
   }, []);
@@ -30,33 +32,86 @@ export default function Banner() {
     return str?.length > n ? str.substr(0, n - 1) + '...' : str;
   };
 
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-        backgroundPosition: 'top center',
-        backgroundSize: 'cover',
-      }}
-    >
-      <div className="banner__contents">
-        {/**Tittle */}
-        <h1 className="banner_title">
-          {movie?.title || movie?.name || movie?.original_name}
-        </h1>
-        <div className="banner__buttons">
-          <button className="banner__button play">Play</button>
-          <button className="banner__button info">
-            <div className="space"></div> More Information
-          </button>
+  if (!isClicked) {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+          backgroundPosition: 'top center',
+          backgroundSize: 'cover',
+        }}
+      >
+        <div className="banner__contents">
+          {/**Tittle */}
+          <h1 className="banner_title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+          <div className="banner__buttons">
+            <button
+              className="banner__button play"
+              onClick={() => setisClicked(true)}
+            >
+              Play
+            </button>
+            <button className="banner__button info">
+              <div className="space"></div> More Information
+            </button>
+          </div>
+          {/**DIV > 2 BUTTONS */}
+          <h1 className="banner__description">
+            {truncate(movie?.overview, 100)}
+          </h1>
+          {/**Description */}
         </div>
-        {/**DIV > 2 BUTTONS */}
-        <h1 className="banner__description">
-          {truncate(movie?.overview, 100)}
-        </h1>
-        {/**Description */}
-      </div>
-      <div className="banner--fadeBottom"></div>
-    </header>
-  );
+        <div className="banner--fadeBottom"></div>
+      </header>
+    );
+  } else {
+    return (
+      <Container>
+        <HomeContainer>
+          <Iframe
+            width="640"
+            height="360"
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="autoplay; fullscreen"
+          ></Iframe>
+        </HomeContainer>
+      </Container>
+    );
+  }
 }
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
