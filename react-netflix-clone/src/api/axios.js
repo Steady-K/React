30d45b2bda1b667370173579cea134e1f3ac9f8a
Steady-K <tkdlxm56@gmail.com>
2025-03-15 15:31:20 +0900

    feat:Banner

--- react-netflix-clone/src/api/axios.js
+++ react-netflix-clone/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://www.themoviedb.org/3',
+  baseURL: 'https://api.themoviedb.org/3',
   params: {
     api_key: '889ef59af1cb2261c7822c8f27728052',
     language: 'ko-kr',
--- react-netflix-clone/src/components/Nav.js
+++ react-netflix-clone/src/components/Nav.js
@@ -22,7 +22,7 @@ export default function Nav() {
     <nav className={`nav ${show && 'nav__black'}`}>
       <img
         alt="Netflix logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Netflix.png"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
         className="nav__logo"
         onClick={() => window.location.reload()}
       />
